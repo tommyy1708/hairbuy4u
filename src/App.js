62480d2a94f86d51db6e8fc6930e6b90f3b17523
tommyy1708 <hzhan081@fiu.edu>
2023-11-13 11:01:31 -0500

    Version_1.2.5    (1)resubmit 1.2.5

--- src/App.js
+++ src/App.js
@@ -86,6 +86,8 @@ const App = () => {
       total: 0,
       casher: localStorage.getItem('username'),
       method: '',
+      total_cost:0,
+      profit: 0,
     });
   };
 
@@ -132,7 +134,7 @@ const App = () => {
     newCart.total += item_total;
     setCartData(newCart);
   };
-  
+
   //minus quantity from shopping cart
   const subItemToCart = (item) => {
     const newCart = { ...cartData };
